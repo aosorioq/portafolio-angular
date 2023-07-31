0e649eef906641c6528dda48dae47a896b83fa84
Alex Osorio <aosorioq@gmail.com>
2023-07-31 16:33:38 -0400

    Fin sección 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,19 +10,35 @@ export class InfoPaginaService {
   info?: InfoPagina;
   cargada=false;
 
+  //aqui
+  equipo:any[]=[];
+  resp=false;
+  
+
+
   constructor( private http: HttpClient) { 
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
-    //console.log('Servicio de Infopagina listo');
-
+  private cargarInfo(){
     //Leer el archivo JSON
-
     this.http.get<InfoPagina>('assets/data/data-pagina.json')
         .subscribe( (resp:InfoPagina) => {
           this.cargada = true;
           this.info = resp;
-          console.log(resp);
-
-        }
-          );
+          });
   }
+
+  private cargarEquipo() {
+    //la base de datos firebase
+    this.http.get('https://angular-html-18157-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe( (resp:any) => {
+      this.equipo = resp as any[];
+      console.log(resp);
+    });
+    
+  }
+
+
 }
